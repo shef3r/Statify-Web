ef5a9da505398c5ea40dc3f57fa482933cf5d902
shef3r <brunorek10@gmail.com>
2024-02-05 14:18:06 +0100

    made the api thing workkkk

---
+++ src/components/TopItemComponent.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let artist;
+    export let title;
+    export let image;
+  </script>
+  
+  <div>
+    <h2>{title} by {artist}</h2>
+    <img src={image} alt="{title} album cover" />
+  </div>
+  
--- src/routes/top/+page.svelte
+++ src/routes/top/+page.svelte
@@ -1,5 +1,7 @@
 <script>
     import NavBar from "../../components/NavBar.svelte";
+    import { onMount } from 'svelte';
+    import TopItemComponent from "../../components/TopItemComponent.svelte";
 
     function getTopMsg() {
         const type = (location.href.split("?type=")[1]).split("&time=")[0];
@@ -18,8 +20,53 @@
         }
         return message;
     }
+
+    async function getTopTracks() {
+        const type = "tracks";
+        const time = (location.href.split("?type=")[1]).split("&time=")[1];
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer " + localStorage.getItem("access_token"));
+
+        var requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+        try {
+            let response = await fetch("https://api.spotify.com/v1/me/top/" + type + "?time_range=" + time + "_term&limit=50&offset=0", requestOptions);
+            let result = await response.json();
+            return result;
+        } catch (error) {
+            console.log('error', error);
+        }   
+    }
+
+
+    let data;
+
+    onMount(async () => {
+        if ((location.href.split("?type=")[1]).split("&time=")[0] == "tracks") {
+            data = await getTopTracks();
+        }
+        else {
+            data = null;
+        }
+    });
+
 </script>
 
 <NavBar/>
 <br>
 <h1 class="text-4xl font-bold text-center" id="glow">{getTopMsg()}</h1>
+
+{#if data}
+  {#each data.items as item (item.id)}
+    <TopItemComponent 
+      artist={item.artists[0].name} 
+      title={item.name} 
+      image={item.album.images[2].url} 
+    />
+  {/each}
+{:else}
+  <p>Loading...</p>
+{/if}
